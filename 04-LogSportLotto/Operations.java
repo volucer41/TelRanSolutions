5fcb8ce780d8616ab9e209e65e78604656c4b5f8
Konstantin <volucer41@gmail.com>
2021-05-31 18:20:40 +0300

    Lesson 04 updated

--- 04-LogSportLotto/Operations.java
+++ 04-LogSportLotto/Operations.java
@@ -13,12 +13,14 @@ public class Operations {
 	public static int log2(long n) {
 		int log2Result = 0;
 		n = (n < 0) ? (~n + 1) : (n);
-		for (int i = 62; i != -1; i--) {
+
+		int i = 62;
+		while (log2Result == 0 && n != 0) {
 			log2Result = getBit(n, i);
 			if (log2Result == 1) {
 				log2Result = i;
-				break;
 			}
+			i--;
 		}
 
 		return log2Result;
--- 04-LogSportLotto/SportLotto.java
+++ 04-LogSportLotto/SportLotto.java
@@ -17,13 +17,11 @@ public class SportLotto {
 	public static void displaySportLotto() {
 
 		long counter = 0;
-		while (counter < 7881299347898368L) {
-			// 7881299347898368L is a same as Math.pow(2 ,50) * 7
+		while (counter < Math.pow(2, 50) * 7) {
 			int number = (int) (1 + Math.random() * 49);
 			if (getBit(counter, number) == 0) {
 				counter = setBit(counter, number);
-				counter += 1125899906842624L;
-				// 1125899906842624L is a same as Math.pow(2 ,50)
+				counter += Math.pow(2, 50);
 				System.out.println(number);
 			}
 		}
